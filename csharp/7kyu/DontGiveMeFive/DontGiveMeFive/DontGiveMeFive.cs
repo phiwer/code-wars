606b9e4649eeb48f484b1563e20ef74ddc7ddc5a
Philip Werner <phiwer@gmail.com>
2017-02-04 21:17:33 +0100

    Refactored for final submission.

---
+++ csharp/7kyu/DontGiveMeFive/DontGiveMeFive/DontGiveMeFive.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DontGiveMeFive
+{
+    using System.Globalization;
+
+    public class Kata
+    {
+        public static int DontGiveMeFive(int start, int end)
+        {
+            IEnumerable<int> region = GenerateRegion(start, end);
+
+            int valueFilter = 5;
+            region = FilterRegion(region, valueFilter);
+
+            return region.Count();
+        }
+
+        public static IEnumerable<int> FilterRegion(IEnumerable<int> region, int valueFilter)
+        {
+            return region.Where(x => !x.ToString().Contains(valueFilter.ToString())).AsEnumerable();
+        }
+
+        public static IEnumerable<int> GenerateRegion(int start, int end)
+        {
+            if (start > end)
+            {
+                Swap(ref start, ref end);
+            }
+
+            return Enumerable.Range(start, end - start + 1);
+        }
+
+        private static void Swap(ref int start , ref int end)
+        {
+            int tmp = start;
+            start = end;
+            end = tmp;
+        }
+    }
+}
---
+++ csharp/7kyu/DontGiveMeFive/DontGiveMeFive/DontGiveMeFiveTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DontGiveMeFive
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class KataTests
+    {
+        [Test]
+        public void When_Distance_Between_Start_End_Is_Greater_Than_Zero_Then_Result_Should_Be_Greater_Than_Zero()
+        {
+            // Arrange
+            int regionStart = 5;
+            int regionEnd = 20;
+
+            // Act
+            int result = Kata.DontGiveMeFive(regionStart, regionEnd);
+
+            // Assert
+            Assert.GreaterOrEqual(result, 1);
+        }
+
+        [Test]
+        public void When_Start_And_End_Are_Negative_Should_Give_Non_Negative_Result()
+        {
+            // Arrange
+            int regionStart = -5;
+            int regionEnd = -7;
+
+            // Act
+            var result = Kata.DontGiveMeFive(regionStart, regionEnd);
+
+            // Assert
+            Assert.GreaterOrEqual(result, 1);
+        }
+
+        [Test]
+        public void When_Five_Is_In_Region_Then_Result_Should_Not_Include_Value()
+        {
+            // Arrange 
+            int regionStart = 4;
+            int regionEnd = 12;
+
+            // Act
+            var result = Kata.DontGiveMeFive(regionStart, regionEnd);
+
+            // Assert
+            Assert.AreEqual(regionEnd - regionStart, result);
+        }
+    }
+}
